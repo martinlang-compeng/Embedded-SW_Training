b1d90eb74edd9104e1ccec199b307cb6530df640
Martin Lang <mlang@evertz.com>
2026-03-29 23:12:37 -0400

    [LEETCODE] Add attemped solution for problem "Valid Anagram"

---
+++ LeetCode/validAnagram.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // If strings are not equal in size, we can return false right away
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        // One method is to keep a hash map for each string
+        // Where the key is each unique letter, and the
+        // value is how many times that letter appears
+
+        // At the end, you compare the hash maps and if they 
+        // are equal return true, if not return false
+
+        std::unordered_map<char, int> s_map;
+        std::unordered_map<char, int> t_map;
+
+        for(unsigned i = 0; i < s.length(); i++) {
+            s_map[s[i]]++;
+            t_map[t[i]]++;
+        }
+
+        if(s_map == t_map)
+            return true;
+        else
+            return false;
+    }
+};
